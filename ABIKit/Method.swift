dcd92bd7224357be75e623bdf0648aec112c1732
Johan Sellström <johan.sellstrom@combient.com>
2017-09-24 19:09:30 +0200

    duh

--- ABIKit/Method.swift
+++ ABIKit/Method.swift
@@ -10,6 +10,101 @@ import Gloss
 import SwiftKeccak
 import EtherKit
 
+
+public func encodeParam(type: String, value: String) -> String? {
+    
+    switch type {
+    case "int","int8","int16","int32","int64","int128","int256":
+        var bn = BigNumber(decimalString: value)!
+        if bn.isNegative {
+            let bn1 = bn.complement()
+            let bn2 = bn1?.add(BigNumber.constantOne())
+            bn = bn2!
+            //bn = bn1!
+        }
+        return truncateAndPad(bn: bn)
+    case "uint","uint8","uint16","uint32","uint64","uint128","uint256": // Tested and working
+        let bn = BigNumber(decimalString: value)!
+        if bn.isNegative { // This is an error
+            return nil
+        }
+        return truncateAndPad(bn: bn)
+    case "address": // Tested and working
+        // just left pad with zeros
+        let bn = BigNumber(hexString: value)!
+        return truncateAndPad(bn: bn)
+    case "string":
+        /* enc(X) = enc(enc_utf8(X)), i.e. X is utf-8 encoded and this value is interpreted as of bytes type and encoded further. Note that the length used in this subsequent encoding is the number of bytes of the utf-8 encoded string, not its number of characters. */
+        
+        let str = value
+        let data = str.data(using: .utf8)!
+        let hexString = data.map{ String(format:"%02x", $0) }.joined()
+        
+        let len = hexString.lengthOfBytes(using: .utf8)
+        let bn = BigNumber(integer: len/2)
+        let p = BigNumber(integer: 32)
+        
+        /*
+         bytes, of length k (which is assumed to be of type uint256):
+         
+         enc(X) = enc(k) pad_right(X), i.e. the number of bytes is encoded as a uint256 followed by the actual value of X as a byte sequence, followed by the minimum number of zero-bytes such that len(enc(X)) is a multiple of 32.*/
+        
+        return truncateAndPad(bn: p!) + truncateAndPad(bn: bn!) + hexString.rightPad()
+    case "bytes":
+        
+        let hexString = value.deHexPrefix()
+        let len = hexString.lengthOfBytes(using: .utf8)
+        
+        print("lengthOfBytes ",len)
+        /*
+         if len = 200
+         160 100
+         0x00000000000000000000000000000000000000000000000000000000000000a0 0000000000000000000000000000000000000000000000000000000000000064
+         
+         */
+        let bn = BigNumber(integer: len/2) // 100
+        
+        let rightPadded = hexString.rightPad()
+        let paddedLen = 64 + rightPadded.lengthOfBytes(using: .utf8)
+        let p = BigNumber(integer: paddedLen/2) //
+        
+        /*
+         bytes, of length k (which is assumed to be of type uint256):
+         
+         enc(X) = enc(k) pad_right(X), i.e. the number of bytes is encoded as a uint256 followed by the actual value of X as a byte sequence, followed by the minimum number of zero-bytes such that len(enc(X)) is a multiple of 32.*/
+        
+        
+        print("bytes ", value, truncateAndPad(bn: p!) +  truncateAndPad(bn: bn!) + hexString.rightPad())
+        return truncateAndPad(bn: p!) +  truncateAndPad(bn: bn!) + hexString.rightPad()
+        
+    case "bytes32":
+        
+        let str = value.deHexPrefix()
+        return str.rightPad32()
+        
+    default:
+        return nil
+    }
+    
+}
+
+public func encodeParams(_ parameterTypes: Array<String>, _ parameterValues: Array<String>) -> String? {
+    
+    var i=0
+    var parameterSignature = ""
+    for parameterType in parameterTypes {
+        
+        let matched = matches(for: "^((u?int|bytes)([0-9]*)|(address|bool|string)|([([0-9]*)]))", in: parameterType)
+        
+        for match in matched {
+            parameterSignature = parameterSignature + encodeParam(type: match, value: parameterValues[i])!
+        }
+        i = i + 1
+    }
+    return parameterSignature.lowercased()
+}
+
+
 struct IOParameter: Glossy {
     
     let indexed: Bool?
@@ -107,209 +202,111 @@ public struct Method: Glossy {
         let signatureHash = (signature.data(using: String.Encoding.utf8)!.keccak()).hexEncodedString()
         let indx = signatureHash.index(signatureHash.startIndex, offsetBy: 8)
         let methodSignature = signatureHash.substring(to:indx)
-        
-        //print("final encoding",  "0x"+methodSignature+parameterSignature(values: values)!)
         return "0x" + methodSignature + encodeParams(parameterTypes, values)!
-        
-
-    }
-    
-    
-    public func encodeParam(type: String, value: String) -> String? {
-        
-        switch type {
-        case "int","int8","int16","int32","int64","int128","int256":
-            var bn = BigNumber(decimalString: value)!
-            if bn.isNegative {
-                let bn1 = bn.complement()
-                let bn2 = bn1?.add(BigNumber.constantOne())
-                bn = bn2!
-                //bn = bn1!
-            }
-            return truncateAndPad(bn: bn)
-        case "uint","uint8","uint16","uint32","uint64","uint128","uint256": // Tested and working
-            let bn = BigNumber(decimalString: value)!
-            if bn.isNegative { // This is an error
-                return nil
-            }
-            return truncateAndPad(bn: bn)
-        case "address": // Tested and working
-            // just left pad with zeros
-            let bn = BigNumber(hexString: value)!
-            return truncateAndPad(bn: bn)
-        case "string":
-            /* enc(X) = enc(enc_utf8(X)), i.e. X is utf-8 encoded and this value is interpreted as of bytes type and encoded further. Note that the length used in this subsequent encoding is the number of bytes of the utf-8 encoded string, not its number of characters. */
-            
-            let str = value
-            let data = str.data(using: .utf8)!
-            let hexString = data.map{ String(format:"%02x", $0) }.joined()
-            
-            let len = hexString.lengthOfBytes(using: .utf8)
-            let bn = BigNumber(integer: len/2)
-            let p = BigNumber(integer: 32)
-            
-            /*
-             bytes, of length k (which is assumed to be of type uint256):
-             
-             enc(X) = enc(k) pad_right(X), i.e. the number of bytes is encoded as a uint256 followed by the actual value of X as a byte sequence, followed by the minimum number of zero-bytes such that len(enc(X)) is a multiple of 32.*/
-            
-            return truncateAndPad(bn: p!) + truncateAndPad(bn: bn!) + hexString.rightPad()
-        case "bytes":
-            
-            let hexString = value.deHexPrefix()
-            let len = hexString.lengthOfBytes(using: .utf8)
-            
-            print("lengthOfBytes ",len)
-            /*
-             if len = 200
-             160 100
-             0x00000000000000000000000000000000000000000000000000000000000000a0 0000000000000000000000000000000000000000000000000000000000000064
-             
-             */
-            let bn = BigNumber(integer: len/2) // 100
-            
-            let rightPadded = hexString.rightPad()
-            let paddedLen = 64 + rightPadded.lengthOfBytes(using: .utf8)
-            let p = BigNumber(integer: paddedLen/2) //
-            
-            /*
-             bytes, of length k (which is assumed to be of type uint256):
-             
-             enc(X) = enc(k) pad_right(X), i.e. the number of bytes is encoded as a uint256 followed by the actual value of X as a byte sequence, followed by the minimum number of zero-bytes such that len(enc(X)) is a multiple of 32.*/
-            
-            
-            print("bytes ", value, truncateAndPad(bn: p!) +  truncateAndPad(bn: bn!) + hexString.rightPad())
-            return truncateAndPad(bn: p!) +  truncateAndPad(bn: bn!) + hexString.rightPad()
-            
-        case "bytes32":
-            
-            let str = value.deHexPrefix()
-            return str.rightPad32()
-            
-        default:
-            return nil
-        }
-        
-    }
-    
-    public func encodeParams(_ parameterTypes: Array<String>, _ parameterValues: Array<String>) -> String? {
-        
-        var i=0
-        var parameterSignature = ""
-        for parameterType in parameterTypes {
-            
-            let matched = matches(for: "^((u?int|bytes)([0-9]*)|(address|bool|string)|([([0-9]*)]))", in: parameterType)
-            
-            for match in matched {
-                parameterSignature = parameterSignature + encodeParam(type: match, value: parameterValues[i])!
-            }
-            i = i + 1
-        }
-        return parameterSignature.lowercased()
-    }
-    
-    
-    func parameterSignature(values: Array<String>) -> String? {
-        
-        var i=0
-        var parameterSignature = ""
-        for input in inputs! {
-            
-            let matched = matches(for: "^((u?int|bytes)([0-9]*)|(address|bool|string)|([([0-9]*)]))", in: input.type)
-            
-            var bn : BigNumber = BigNumber()
-            
-            for match in matched {
-                
-                switch match {
-                case "int","int8","int16","int32","int64","int128","int256": // Probably works now for both positive and negative
-                    
-                    bn = BigNumber(decimalString: values[i])
-                    if bn.isNegative {
-                        
-                        let bn1 = bn.complement()
-                        let bn2 = bn1?.add(BigNumber.constantOne())
-                        bn = bn2!
-                    }
-                    parameterSignature = parameterSignature + truncateAndPad(bn: bn)
-                    break
-                case "uint","uint8","uint16","uint32","uint64","uint128","uint256": // Tested and working
-                    bn = BigNumber(decimalString: values[i])
-                    if bn.isNegative { // This is an error
-                        return nil
-                    }
-                    parameterSignature = parameterSignature + truncateAndPad(bn: bn)
-                    break
-                case "address": // Tested and working
-                    // just left pad with zeros
-                    bn = BigNumber(hexString: values[i])
-                    parameterSignature = parameterSignature + truncateAndPad(bn: bn)
-                    break
-                    
-                case "string":
-                    
-                    // let utf8 = String(values[i].utf8)
-                    
-                    /* enc(X) = enc(enc_utf8(X)), i.e. X is utf-8 encoded and this value is interpreted as of bytes type and encoded further. Note that the length used in this subsequent encoding is the number of bytes of the utf-8 encoded string, not its number of characters. */
-                    
-                    let str = values[i]
-                    let data = str.data(using: .utf8)!
-                    let hexString = data.map{ String(format:"%02x", $0) }.joined()
-                    
-                    let startIndex = hexString.index((hexString.startIndex), offsetBy: 2)
-                    let truncated = hexString.substring(from: startIndex)
-                    
-                    /*
-                     bytes, of length k (which is assumed to be of type uint256):
-                     
-                     enc(X) = enc(k) pad_right(X), i.e. the number of bytes is encoded as a uint256 followed by the actual value of X as a byte sequence, followed by the minimum number of zero-bytes such that len(enc(X)) is a multiple of 32.*/
-                    
-                    parameterSignature = parameterSignature + truncated.rightPad()
-                    
-                    break
-                case "bytes":
-                    
-                    let hexString = values[i]
-                    
-                    /*let str = values[i]
-                     let data = str.data(using: .utf8)!
-                     let hexString = data.map{ String(format:"%02x", $0) }.joined()
-                     */
-                    
-                    let startIndex = hexString.index((hexString.startIndex), offsetBy: 2)
-                    let truncated = hexString.substring(from: startIndex)
-                    
-                    /*
-                     bytes, of length k (which is assumed to be of type uint256):
-                     
-                     enc(X) = enc(k) pad_right(X), i.e. the number of bytes is encoded as a uint256 followed by the actual value of X as a byte sequence, followed by the minimum number of zero-bytes such that len(enc(X)) is a multiple of 32.*/
-                    
-                    parameterSignature = parameterSignature + truncated.rightPad()
-                    break
-                case "bytes32":
-                    
-                    var hexString:String
-                    
-                    let str = values[i]
-                    let index = str.index((str.startIndex), offsetBy: 2)
-                    let prefix = str.substring(to: index)
-                    
-                    if prefix == "0x" {
-                        let startIndex = str.index((str.startIndex), offsetBy: 2)
-                        hexString = str.substring(from: startIndex)
-                    } else {
-                        hexString = str
-                    }
-                    
-                    parameterSignature = parameterSignature + hexString.rightPad32()                    
-                    break
-                default: break
-                }
-                i = i + 1
-            }
-        }
-        return parameterSignature
     }
 }
 
+/*
 
+func parameterSignature(values: Array<String>) -> String? {
+    
+    var i=0
+    var parameterSignature = ""
+    for input in inputs! {
+        
+        let matched = matches(for: "^((u?int|bytes)([0-9]*)|(address|bool|string)|([([0-9]*)]))", in: input.type)
+        
+        var bn : BigNumber = BigNumber()
+        
+        for match in matched {
+            
+            switch match {
+            case "int","int8","int16","int32","int64","int128","int256": // Probably works now for both positive and negative
+                
+                bn = BigNumber(decimalString: values[i])
+                if bn.isNegative {
+                    
+                    let bn1 = bn.complement()
+                    let bn2 = bn1?.add(BigNumber.constantOne())
+                    bn = bn2!
+                }
+                parameterSignature = parameterSignature + truncateAndPad(bn: bn)
+                break
+            case "uint","uint8","uint16","uint32","uint64","uint128","uint256": // Tested and working
+                bn = BigNumber(decimalString: values[i])
+                if bn.isNegative { // This is an error
+                    return nil
+                }
+                parameterSignature = parameterSignature + truncateAndPad(bn: bn)
+                break
+            case "address": // Tested and working
+                // just left pad with zeros
+                bn = BigNumber(hexString: values[i])
+                parameterSignature = parameterSignature + truncateAndPad(bn: bn)
+                break
+                
+            case "string":
+                
+                // let utf8 = String(values[i].utf8)
+                
+                /* enc(X) = enc(enc_utf8(X)), i.e. X is utf-8 encoded and this value is interpreted as of bytes type and encoded further. Note that the length used in this subsequent encoding is the number of bytes of the utf-8 encoded string, not its number of characters. */
+                
+                let str = values[i]
+                let data = str.data(using: .utf8)!
+                let hexString = data.map{ String(format:"%02x", $0) }.joined()
+                
+                let startIndex = hexString.index((hexString.startIndex), offsetBy: 2)
+                let truncated = hexString.substring(from: startIndex)
+                
+                /*
+                 bytes, of length k (which is assumed to be of type uint256):
+                 
+                 enc(X) = enc(k) pad_right(X), i.e. the number of bytes is encoded as a uint256 followed by the actual value of X as a byte sequence, followed by the minimum number of zero-bytes such that len(enc(X)) is a multiple of 32.*/
+                
+                parameterSignature = parameterSignature + truncated.rightPad()
+                
+                break
+            case "bytes":
+                
+                let hexString = values[i]
+                
+                /*let str = values[i]
+                 let data = str.data(using: .utf8)!
+                 let hexString = data.map{ String(format:"%02x", $0) }.joined()
+                 */
+                
+                let startIndex = hexString.index((hexString.startIndex), offsetBy: 2)
+                let truncated = hexString.substring(from: startIndex)
+                
+                /*
+                 bytes, of length k (which is assumed to be of type uint256):
+                 
+                 enc(X) = enc(k) pad_right(X), i.e. the number of bytes is encoded as a uint256 followed by the actual value of X as a byte sequence, followed by the minimum number of zero-bytes such that len(enc(X)) is a multiple of 32.*/
+                
+                parameterSignature = parameterSignature + truncated.rightPad()
+                break
+            case "bytes32":
+                
+                var hexString:String
+                
+                let str = values[i]
+                let index = str.index((str.startIndex), offsetBy: 2)
+                let prefix = str.substring(to: index)
+                
+                if prefix == "0x" {
+                    let startIndex = str.index((str.startIndex), offsetBy: 2)
+                    hexString = str.substring(from: startIndex)
+                } else {
+                    hexString = str
+                }
+                
+                parameterSignature = parameterSignature + hexString.rightPad32()
+                break
+            default: break
+            }
+            i = i + 1
+        }
+    }
+    return parameterSignature
+}
+*/
